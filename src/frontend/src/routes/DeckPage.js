501eb25cfc51c204169f4c27f69eb398f1f470d7
hnton211 <hnton211@gmail.com>
2022-01-16 14:23:11 +0700

    fix

--- src/frontend/src/routes/DeckPage.js
+++ src/frontend/src/routes/DeckPage.js
@@ -1,27 +1,53 @@
 import React, { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
-import { getWordsFromDeck } from "../apis/flashcardModel";
-import FlashcardList from "../components/DeckPage/FlashcardList";
+import { getWordsFromDeck, pushToDeck } from "../apis/flashcardModel";
+import Flashcard from "../components/DeckPage/Flashcard";
 
 export default function DeckPage() {
   let history = useHistory();
   const { id } = useParams();
-  const [wordList, setWordList] = useState(undefined);
+  const [wordList, setWordList] = useState([]);
+
+  const fetchData = async () => {
+    const { data } = await getWordsFromDeck(id);
+    setWordList(data);
+  };
 
   const handleClick = () => {
     history.push(`/deck/${id}/add`);
   };
 
-  useEffect(async () => {
-    const { data } = await getWordsFromDeck(id);
-    setWordList(data);
+  useEffect(() => {
+    fetchData();
   }, []);
+
+  const handleLevelSubmit = async (level) => {
+    const { data } = await pushToDeck(id, wordList[0].id, level);
+    fetchData();
+  };
 
   return (
     <div>
-      <div className="container">
-        {wordList && <FlashcardList flashcards={wordList} />}
+      <div className="card-window">
+        {wordList.length > 0 && <Flashcard flashcard={wordList[0]} />}
       </div>
+      {wordList.length > 0 && (
+        <div>
+          {" "}
+          <button className="level-button" onClick={() => handleLevelSubmit(1)}>
+            Easy
+          </button>
+          <button className="level-button" onClick={() => handleLevelSubmit(2)}>
+            Normal
+          </button>
+          <button className="level-button" onClick={() => handleLevelSubmit(3)}>
+            Difficult
+          </button>
+          <button className="level-button" onClick={() => handleLevelSubmit(4)}>
+            Legendary
+          </button>
+        </div>
+      )}
       <button title="Add new word" className="add-button" onClick={handleClick}>
         +
       </button>
